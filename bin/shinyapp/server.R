e4a84190ea31a6424a2fb87fba8f6873376d3d35
jaclyn.matthes <jaclyn.hatala.matthes@gmail.com>
2018-06-06 16:25:04 -0400

    First push mammal traits app to QUBES

--- bin/shinyapp/server.R
+++ bin/shinyapp/server.R
@@ -1,161 +1,164 @@
 library(shiny)
 library(ggplot2)
-library(reshape2)
 
-new.p <- function(p, w11, w12, w22){
-  wbar <- w11*p^2 + 2*w12*p*(1-p) + w22*(1-p)^2
-  p.change <- (p*(1-p)/wbar)*(p*(w11-w12) - (1-p)*(w22-w12))
-  return(p + p.change)
-}
+#Download data from ESA URL
+dat_url <- "http://esapubs.org/archive/ecol/E090/184/PanTHERIA_1-0_WR05_Aug2008.txt"
+dat  <- data.frame(read.delim(dat_url,header=TRUE, sep="\t",na.strings="-999.00"))
+dat$metmass <- dat$X18.1_BasalMetRate_mLO2hr / dat$X5.1_AdultBodyMass_g
+dat$species <- paste(dat$MSW05_Genus,dat$MSW05_Species,sep=" ")
+order.names <- unique(dat[,1])
 
-pheno.freqs <- function(p){
-  return(c(p^2, 2*p*(1-p), (1-p)^2))
-}
+#Key for labels
+lab_key <- list()
+lab_key[[56]] <- "Metabolic Rate (mL O2/ g / hr)"
+lab_key[[7]] <- "Body Mass (g)"
+lab_key[[16]] <- "Gestational Duration (days)"
+lab_key[[18]] <- "Home range (km2)"
+lab_key[[21]] <- "Litter size (n)"
+lab_key[[23]] <- "Maximum longevity (months)"
+lab_key[[24]] <- "Neonate mass (g)"
+lab_key[[26]] <- "Population density (n/km2)"
+lab_key[[29]] <- "Social group size (n)"
+lab_key[[28]] <- "Sexual maturity age (days)"
+lab_key[[33]] <- "Weaning age (days)"
+lab_key[[52]] <- "Mean annual precipitation (mm)"
+lab_key[[53]] <- "Mean annual temperature (0.1 C)"
 
+#Key for table
+tab_key <- list()
+tab_key[[56]] <- "Met Rate"
+tab_key[[7]] <- "Mass"
+tab_key[[16]] <- "Gest Dur"
+tab_key[[18]] <- "Range"
+tab_key[[21]] <- "Litt size"
+tab_key[[23]] <- "Max long"
+tab_key[[24]] <- "Neo mass"
+tab_key[[26]] <- "Pop Dens"
+tab_key[[29]] <- "Group size"
+tab_key[[28]] <- "Mat Age"
+tab_key[[33]] <- "Wean Age"
+tab_key[[52]] <- "MAP"
+tab_key[[53]] <- "MAT"
 
 shinyServer(
   function(input, output){
     
-    theme_set(theme_minimal(base_size = 16))
-    
-    # Random genetic drift panel
-    output$driftplot <- renderPlot({
-      # Set up parameters
-      N = input$N # number of diploid individuals
-      N.chrom = 2*N # number of chromosomes
-      p = input$p; q = 1-p
-      N.gen = input$N.gen # number of generations
-      N.sim = input$N.sim  # number of simulations
-      
-      # Simulation
-      X <- array(0, dim=c(N.gen,N.sim))
-      X[1,] = rep(N.chrom*p,N.sim) # initialize number of A1 alleles in first generation
-      for(j in 1:N.sim){
-        for(i in 2:N.gen){
-          X[i,j] = rbinom(1,N.chrom,prob=X[i-1,j]/N.chrom)
-        }  
-      }
-      X <- data.frame(X/N.chrom)
-      colnames(X) <- seq(1:N.sim)
-      
-      # Reshape data and plot the 5 simulations
-      sim_data <- melt(X)
-      ggplot(sim_data, 
-             aes(x = rep(c(1:N.gen), N.sim), y = value, color = variable)) + 
-        geom_line() + 
-        xlab("Generation") + 
-        ylab("A Allele Frequency") + ylim(0,1) + 
-        labs(colour = "Simulations")
-    })
-    
-    output$island1 <- renderPlot({
-      
-      # CONNECTED ISLAND MODEL
-      sim.island <- function(q, migration.rate, num.gens){
-        num.islands <- length(q)
-        q.history <- data.frame(generation=numeric(), island=numeric(), q=numeric())
-        q.history[1:num.islands, ] <- cbind(rep(0, num.islands), 1:num.islands, q)
+    output$myplot <- renderPlot({
+      if(input$order=="All"){
         
-        for (n in 1:num.gens){
-          q.last <- q
-          for(i in 1:num.islands){
-            q[i] <- (1-migration.rate[n])*q.last[i] + migration.rate[n]*(mean(q.last[-i]))
-          }
-          q.history[n*num.islands+(1:num.islands),] <- cbind(rep(n, num.islands), 1:num.islands, q)
+        colx <-  dat[,as.numeric(input$xvar)]
+        coly <-  dat[,as.numeric(input$yvar)]
+        Order <-  dat[,1]
+        Species <-  dat$species
+        dat_new <-  data.frame(colx,coly,Order,Species)
+        dat_new <-  dat_new[complete.cases(dat_new),]
+        
+        if(input$fitdata=="Linear"){
+          g <-  ggplot(dat_new, aes(x=colx, y=coly, col=Order)) + geom_point() +
+            xlab(lab_key[[as.numeric(input$xvar)]]) + 
+            ylab(lab_key[[as.numeric(input$yvar)]]) +
+            scale_x_log10() + scale_y_log10() + 
+            theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold"),
+                  legend.text=element_text(size=14),legend.title=element_text(size=14,face="bold"))+
+            geom_smooth(aes(group=1),method="lm",color="black",se=TRUE) 
+        } else if (input$fitdata=="None"){
+          g <-  ggplot(dat_new, aes(x=colx, y=coly, col=Order)) + geom_point() +
+            xlab(lab_key[[as.numeric(input$xvar)]]) + ylab(lab_key[[as.numeric(input$yvar)]]) +
+            scale_x_log10() + scale_y_log10() + 
+            theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold"),
+                  legend.text=element_text(size=14),legend.title=element_text(size=14,face="bold")) 
         }
-        q.history$island <- as.factor(q.history$island)
-        return(q.history)
-      }
-      q0 <- c(input$af1, input$af2)
-      migration.rate <- c(rep(0, input$t_connect),rep(input$migration, 50-input$t_connect))
-      results <- sim.island(q=q0, migration.rate = migration.rate, num.gens = 50)
-      ggplot(results, aes(generation, q, group=island)) +
-        geom_line(aes(color=island)) +
-        labs(y = "A Allele Frequency", x = "Generation") +
-        scale_color_discrete(name = "Island")
-    })
-    
-    output$natselect <- renderPlot({
-      # DIPLOID SELECTION
-      plot.allele.history <- function(p, w11, w12, w22, gens=100){
-        
-        history <- data.frame(generation=numeric(), 
-                              cat=character(), Type=character(), frequency=numeric())
-        
-        for (i in 1:gens){
-          phenos <- data.frame(generation=rep(i,3), cat=rep("Phenotypes",3), 
-                               Type=c("AA", "Aa", "aa"), freq=pheno.freqs(p))
-          alleles <- data.frame(generation=rep(i,2), cat=rep("Alleles",2), 
-                                Type=c("A", "a"), freq=c(p, 1-p))
-          history <- rbind(history, phenos,alleles)
-          p <- new.p(p=p, w11=w11, w12=w12, w22=w22)
+      } else {
+        order_rows <-  which(dat[,1]==input$order)
+        colx <-  dat[order_rows,as.numeric(input$xvar)]
+        coly <-  dat[order_rows,as.numeric(input$yvar)]
+        Family <-  dat[order_rows,2]
+        Species <-  dat$species[order_rows]
+        dat_new <-  data.frame(colx,coly,Family,Species)
+        dat_new <-  dat_new[complete.cases(dat_new),]
+        if(input$fitdata=="Linear"){
+          g <-  ggplot(dat_new, aes(x=colx, y=coly, col=Family)) + geom_point()+
+            xlab(lab_key[[as.numeric(input$xvar)]]) + ylab(lab_key[[as.numeric(input$yvar)]])+
+            scale_x_log10() + scale_y_log10()+ 
+            theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold"),
+                  legend.text=element_text(size=14),legend.title=element_text(size=14,face="bold"))+
+            geom_smooth(aes(group=1),method="lm",color="black",se=TRUE) 
+        } else if (input$fitdata=="None"){
+          g <-  ggplot(dat_new, aes(x=colx, y=coly, col=Family)) + geom_point()+
+            xlab(lab_key[[as.numeric(input$xvar)]]) + ylab(lab_key[[as.numeric(input$yvar)]])+
+            scale_x_log10() + scale_y_log10()+ 
+            theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold"),
+                  legend.text=element_text(size=14),legend.title=element_text(size=14,face="bold"))
         }
-        
-        g <- ggplot(history, aes(generation, freq, color=Type)) +
-          geom_line()+ylim(c(0,1)) +
-          facet_grid(cat~.) + 
-          labs(y = "Frequency", x = "Generation")
-        
-        return(g)
       }
-      plot.allele.history(p=input$p_ns, 
-                          w11=input$w11, w12=input$w12, 
-                          w22=input$w22)
+      
+      g
     })
     
-    output$all <- renderPlot({
-      # Set up parameters
-      N <- c(input$popsize_1, input$popsize_2) # number of diploid individuals
-      N.chrom <- 2*N # number of chromosomes
-      
-      sim.island <- function(q, w11, w12, w22, migration.rate, num.gens){
-        num.islands <- length(q)
-        history <- data.frame(generation=numeric(), island=character(), 
-                              cat=character(), Type=character(), frequency=numeric())
-        
-        for (n in 1:num.gens){
-          q.last <- q
-          for(i in 1:num.islands){
-            q.drift <- rbinom(1, N.chrom[i], prob=q.last[i])/N.chrom[i]
-            q.flow <- (1-migration.rate[n])*q.drift + migration.rate[n]*(mean(q.last[-i]))
-            q.select <- new.p(p=q.flow, w11=w11[i], w12=w12[i], w22=w22[i])
-            q[i] <- q.select
-          }
+    # Output linear model equation below graph
+    output$equation <- renderUI({
+      if(input$fitdata=="Linear"){
+        if(input$order=="All"){
+          colx <-  dat[,as.numeric(input$xvar)]
+          coly <-  dat[,as.numeric(input$yvar)]
+          dat_new <-  data.frame(colx,coly)
+          colnames(dat_new) <- c(tab_key[[as.numeric(input$xvar)]],
+                                 tab_key[[as.numeric(input$yvar)]])
+          dat_new <-  dat_new[complete.cases(dat_new),]
+          model <- lm(log(coly) ~ log(colx))
+          slope <- signif(as.numeric(coef(model)[2]),3)
+          int <- signif(as.numeric(coef(model)[1]), 3)
+          r_sq <- signif(as.numeric(summary(model)$adj.r.squared),2)
+          str1 <- paste("ln(",tab_key[[as.numeric(input$yvar)]], 
+                        ") = ",slope,"ln(", 
+                        tab_key[[as.numeric(input$xvar)]], ") + ",int)
+          str2 <- paste(" where R2 = ", r_sq, " with ", nrow(dat_new), 
+                        " unique data points.")
           
-          phenos <- data.frame(generation=rep(n,3*num.islands),
-                               island=rep(paste("Island",as.character(1:num.islands),sep=" "), 3),
-                               cat=rep("Phenotypes",3*num.islands), 
-                               Type=rep(c("AA", "Aa", "aa"), each = num.islands), 
-                               freq=pheno.freqs(q))
+          HTML(paste(str1, str2, sep = '<br/>'))
+        } else {
+          order_rows <-  which(dat[,1]==input$order)
+          colx <-  dat[order_rows,as.numeric(input$xvar)]
+          coly <-  dat[order_rows,as.numeric(input$yvar)]
+          dat_new <-  data.frame(colx,coly)
+          dat_new <-  dat_new[complete.cases(dat_new),]
+          model <- lm(log(coly) ~ log(colx))
+          slope <- signif(as.numeric(coef(model)[2]),3)
+          int <- signif(as.numeric(coef(model)[1]), 3)
+          r_sq <- signif(as.numeric(summary(model)$adj.r.squared),2)
+          str1 <- paste("ln(",tab_key[[as.numeric(input$yvar)]], 
+                        ") = ",slope,"ln(", 
+                        tab_key[[as.numeric(input$xvar)]], ") + ",int)
+          str2 <- paste(" where R2 = ", r_sq, " with ", nrow(dat_new), 
+                        " unique data points.")
           
-          alleles <- data.frame(generation=rep(n,2*num.islands), 
-                                island=rep(paste("Island",as.character(1:num.islands),sep=" "), 2),
-                                cat=rep("Alleles",2*num.islands), 
-                                Type=rep(c("A", "a"), each = num.islands), 
-                                freq=c(q, 1-q))
-          
-          history <- rbind(history, phenos, alleles)
+          HTML(paste(str1, str2, sep = '<br/>'))
         }
-        return(history)
+      } else {
+        paste0("Select 'Linear model' in the pull-down menu to the left to fit a linear model to the log-transformed data.")
       }
-      
-      w11 <- c(input$w11_1, input$w11_2, input$w11_3)
-      w12 <- c(input$w12_1, input$w12_2, input$w12_3)
-      w22 <- c(input$w22_1, input$w22_2, input$w22_3)
-      q0  <- c(input$p1_all, input$p2_all, input$p3_all)
-      
-      migration_rate <- c(rep(0, input$t_connect2),rep(input$migration2, 100-input$t_connect2))
-      results <- sim.island(q=q0, w11=w11, w12=w12, w22=w22,
-                            migration.rate = migration_rate, num.gens = 100)
-      
-      ggplot(results, mapping = aes(generation, freq, color=Type)) +
-        geom_line() +
-        ylim(c(0,1)) +
-        facet_grid(cat~island) + 
-        labs(y = "Frequency", x = "Generation")
     })
     
-    
+    # Output selected points info
+    output$info <- renderPrint({
+      if(input$order=="All"){
+        colx <-  dat[,as.numeric(input$xvar)]
+        coly <-  dat[,as.numeric(input$yvar)]
+        Order <-  dat[,1]
+        Species <-  dat$species
+        dat_new <- data.frame(colx,coly,Order,Species)
+        dat_new <- dat_new[complete.cases(dat_new),]
+        
+      } else {
+        order_rows <-  which(dat[,1]==input$order)
+        colx <-  dat[order_rows,as.numeric(input$xvar)]
+        coly <-  dat[order_rows,as.numeric(input$yvar)]
+        Family <-  dat[order_rows,2]
+        Species <-  dat$species[order_rows]
+        dat_new <-  data.frame(colx,coly,Family,Species)
+        dat_new <-  dat_new[complete.cases(dat_new),]
+      }
+      brushedPoints(dat_new, input$plot_brush)
+    })
   }
 )
-
